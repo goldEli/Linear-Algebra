bf5a4a3866650bf27d917e514a43d7150b68cd50
缪宇 <miaoyu@sefonsoft.com>
2020-09-15 19:52:06 +0800

    add some matrix method

--- main_matrix.py
+++ main_matrix.py
@@ -1,5 +1,10 @@
 from playLA.Matrix import Matrix
 
 if "__main__" == __name__:
-    matrix = Matrix([[1,2], [3,4]])
+    matrix = Matrix([[1,2, 3], [3,4]])
     print(matrix)
+
+    print("matrix.shape = {}".format(matrix.shape()))
+    print("matrix.size = {}".format(matrix.size()))
+    print("len(matrix) = {}".format(len(matrix)))
+    print("matrix[0][0] = {}".format(matrix[0, 0]))
--- playLA/Matrix.py
+++ playLA/Matrix.py
@@ -1,6 +1,37 @@
+from .Vector import Vector
+
 class Matrix:
     def __init__(self, list2d):
         self._values = [row[:] for row in list2d]
 
     def __repr__(self):
         return "Matix({})".format(self._values)
+
+    __str__ = __repr__
+
+    def shape(self):
+        return len(self._values), len(self._values[0])
+
+    def size(self):
+        r, c = self.shape()
+        return r * c
+
+    def __len__(self):
+        return len(self._values)
+
+    def __getitem__(self, item):
+        r, c = item
+        return self._values[r][c]
+
+    def row_vector(self, index):
+        return Vector(self._values[index])
+
+    def column_vector(self, index):
+        return Vector([row[index] for row in self._values])
+
+    def row_num(self):
+        return len(self._values)
+
+    def column_num(self):
+        return len(self._values[0])
+
